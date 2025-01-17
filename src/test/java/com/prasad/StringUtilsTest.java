97e29cf1ddc65b8f0f14df62ba98905e8c15631a
Prasad <prasad.choudhary@humancloud.co.in>
2025-01-17 16:29:29 +0530

    setting up for tests

--- src/test/java/com/prasad/StringUtilsTest.java
+++ src/test/java/com/prasad/StringUtilsTest.java
@@ -1,10 +1,19 @@
 package com.prasad;
 
 
+import com.prasad.impl.StringUtilImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class StringUtilsTest {
+
+  private StringUtil stringUtil;
+
+  @BeforeEach
+  void setUp() {
+    stringUtil = new StringUtilImpl();
+  }
 
   @Test
     public void testForNullInput() {
