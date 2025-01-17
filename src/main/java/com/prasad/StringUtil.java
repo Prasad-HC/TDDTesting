37618f63761e492cd416f9fe587027a7cd0410c2
Prasad <prasad.choudhary@humancloud.co.in>
2025-01-17 16:25:54 +0530

    adding junit for testing

---
+++ src/main/java/com/prasad/StringUtil.java
@@ -0,0 +1,6 @@
+package com.prasad;
+
+public interface StringUtil {
+
+    public Double sumOfDigits();
+}
---
+++ src/test/java/com/prasad/StringUtilsTest.java
@@ -0,0 +1,13 @@
+package com.prasad;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class StringUtilsTest {
+
+  @Test
+    public void testForNullInput() {
+
+    }
+}
