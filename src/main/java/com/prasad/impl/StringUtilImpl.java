174d336f493b825cd5e84bc1b4a7d9363f4b8c66
Prasad <prasad.choudhary@humancloud.co.in>
2025-01-17 16:39:20 +0530

    empty string test passed by implimentation

--- src/main/java/com/prasad/impl/StringUtilImpl.java
+++ src/main/java/com/prasad/impl/StringUtilImpl.java
@@ -7,7 +7,7 @@ public class StringUtilImpl implements StringUtil {
 
     @Override
     public Double sumOfDigits(String str) {
-        if (str == null) {
+        if (str == null || str.length() == 0) {
             throw new IllegalArgumentException("Input string cannot be null");
         }
         return 0.0;
