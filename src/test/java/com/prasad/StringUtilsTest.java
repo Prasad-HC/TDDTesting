d773c29598aad0f292213f9f5c859a1da71344ce
Prasad <prasad.choudhary@humancloud.co.in>
2025-01-17 16:34:30 +0530

    test failed for null argument

--- src/test/java/com/prasad/StringUtilsTest.java
+++ src/test/java/com/prasad/StringUtilsTest.java
@@ -2,8 +2,11 @@ package com.prasad;
 
 
 import com.prasad.impl.StringUtilImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
 
 
 public class StringUtilsTest {
@@ -17,6 +20,6 @@ public class StringUtilsTest {
 
   @Test
     public void testForNullInput() {
-
+    assertThrows(IllegalArgumentException.class, () -> stringUtil.sumOfDigits(null));
     }
 }
